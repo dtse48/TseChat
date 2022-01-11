3c30d6c394c2f1410f0ad21d2e66e53cb03799e3
danieltse <danielts@bu.edu>
2022-01-11 10:52:06 -0600

    installed libraries, implemented hardcoded message-sending

---
+++ server.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname,'public')));
+io.on('connection', socket => {
+    socket.emit('message', 'welcome to TseChat');
+    socket.broadcast.emit('message','A user has joined the chat');
+    socket.on('disconnect', () => {
+        io.emit('message','A user has left the chat');
+    })
+    socket.on('chatMessage', (msg) => {
+        io.emit('message',msg);
+    })
+});
+const PORT = 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
